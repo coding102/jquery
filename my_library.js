6b6264bf867f40d2a521dfc777c3558409d3a43c
Marck <mercercountynj@gmail.com>
2015-12-07 19:58:32 -0500

    initial setup

---
+++ my_library.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+    $("button").click(function(){
+        $("p").toggle();
+    });
+});
+
+
+$(document).ready(function(){
+    $("button").click(function(){
+        $("#header").fadeIn();
+        $("#column").fadeIn("slow");
+        $("#footer").fadeIn(3000);
+    });
+
+
+
+
